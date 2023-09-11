5dfa4a6af249882c136acc68877b6bc28a54b06a
leevccc <94272011+leevccc@users.noreply.github.com>
2023-09-11 21:57:51 +0800

    initial: springboot

---
+++ src/main/java/com/wcf/server/WatchCaseFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.wcf.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchCaseFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchCaseFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wcf/server/WatchCaseFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcf.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchCaseFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
